7ff661b1c5e5ed8fcf7f8d9f3d07618bd6eff4fd
Syailendra M. Ginting <enggaweb@gmail.com>
2021-11-23 21:12:02 +0100

    Minor fixes on layout and certain landmarks

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,5 @@
 <template>
-  <header role="banner">
-    <div class="container">
-      <SiteLogo />
-    </div>
-  </header>
+  <div class="container">
+    <SiteLogo />
+  </div>
 </template>
-
-<style scoped>
-header {
-  padding: 2rem 0;
-  background-color: #efefef;
-  color: white;
-  margin-bottom: 2rem;
-  border-bottom: .5rem solid #80ED99;
-}
-</style>
